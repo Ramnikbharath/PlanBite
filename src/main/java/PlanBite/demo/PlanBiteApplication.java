be9d91385484c545c7766c9cbccbcaf71658a22d
RichardUri <59550956+RichardUri@users.noreply.github.com>
2025-02-01 19:16:58 -0500

    Setting up PlanBite used spring initializer

---
+++ src/main/java/PlanBite/demo/PlanBiteApplication.java
@@ -0,0 +1,13 @@
+package PlanBite.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanBiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanBiteApplication.class, args);
+	}
+
+}
